8ccbda1e3b1ccee817a4df4ec623f90ff08eaac9
Khoa Nguyen <khoa002@gmail.com>
2017-12-05 20:30:42 -0700

    Ignore intellij config files, update index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
 $ini = parse_ini_file('.env');
-var_dump($ini);
- ?>
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?= $ini["APP_NAME"]; ?></title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
+          type="text/css"/>
+</head>
+<body>
+
+</body>
+</html>
